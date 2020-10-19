1eaf3765aff53aecf0afd7c32ec38bd2c6b9b579
ankitcagarwal <ankitcagarwal@gmail.com>
2020-10-18 23:41:43 -0400

    Changed client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,7 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return 1;//this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
